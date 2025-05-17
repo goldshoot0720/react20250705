cc997325af4f2450615280f4d796cba226eec2be
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-17 15:58:42 +0800

    react20250705 great cloud

--- src/mytablecontent9.jsx
+++ src/mytablecontent9.jsx
@@ -1,5 +1,13 @@
 export const mytablecontent9 = [
   {
+    column1: "Google Drive",
+    column2: "15GB",
+    column3: "g--------0720",
+    title: "Google Drive",
+    alt: "Google Drive",
+    link: "https://drive.google.com/",
+  },
+  {
     column1: "pcloud",
     column2: "20GB",
     column3: "g--------0720",
@@ -8,12 +16,51 @@ export const mytablecontent9 = [
     link: "https://www.pcloud.com",
   },
   {
+    column1: "MEGA",
+    column2: "20GB",
+    column3: "g--------0720",
+    title: "MEGA",
+    alt: "MEGA",
+    link: "https://mega.nz",
+  },
+  {
+    column1: "InfiniCLOUD",
+    column2: "48GB",
+    column3: "g--------0720",
+    title: "InfiniCLOUD",
+    alt: "InfiniCLOUD",
+    link: "https://infini-cloud.net",
+  },
+  {
+    column1: "OneDrive",
+    column2: "15GB",
+    column3: "c-----g",
+    title: "OneDrive",
+    alt: "OneDrive",
+    link: "https://onedrive.live.com",
+  }  {
+    column1: "Google Drive",
+    column2: "15GB",
+    column3: "a----17",
+    title: "Google Drive",
+    alt: "Google Drive",
+    link: "https://drive.google.com/",
+  },
+  {
     column1: "pcloud",
     column2: "7GB",
     column3: "a----17",
     title: "pcloud",
     alt: "pcloud",
     link: "https://www.pcloud.com",
+  },
+  {
+    column1: "MEGA",
+    column2: "20GB",
+    column3: "a----17",
+    title: "MEGA",
+    alt: "MEGA",
+    link: "https://mega.nz",
   },
 ];
 
